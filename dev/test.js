0dab7e2baed9d0ead4c8d9cb878868c677e9b8df
Cole Challand <ctchalland@gmail.com>
2018-06-29 08:05:37 -0500

    Tested the Genesis Block

--- dev/test.js
+++ dev/test.js
@@ -2,23 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'OINAISDFN09N09ASDNF90N90ASNF';
-const currentBlockData = [
-  {
-    amount: 101,
-    sender: 'N90ANS90N90ANSDFN',
-    recipient: '90NA90SNDF90ANSDF09N'
-  },
-  {
-    amount: 30,
-    sender: '09ANS09DFNA8SDNF',
-    recipient: 'UIANSIUDFUIABSDUIFB'
-  },
-  {
-    amount: 200,
-    sender: '89ANS89DFN98ASNDF89',
-    recipient: 'AUSDF89ANSD9FNASD'
-  }
-];
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(bitcoin);
