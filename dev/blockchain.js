b4fe890f95e4fab9b5cf5f1055401003670f2c6d
Cole Challand <ctchalland@gmail.com>
2018-06-29 07:42:03 -0500

    Added hashBlock function using sha256

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -7,6 +7,7 @@
 //   //...
 // }
 
+const sha256 = require('sha256');
 
 function Blockchain() {
   this.chain = [];
@@ -42,6 +43,12 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
 
   this.pendingTransactions.push(newTransaction);
   return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+  const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
 }
 
 
