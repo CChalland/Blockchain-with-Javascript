a826ad26ad7290ae137f81b344cbe95745b65258
Cole Challand <ctchalland@gmail.com>
2019-05-11 18:15:52 -0500

    Testing the Genesis block

--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,4 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
 // Testing the blockchain function createNewBlock
@@ -20,33 +19,34 @@ const bitcoin = new Blockchain();
 
 
 // Creating some values to use with when testing the hashBlock function
-const previousBlockHash = 'AFHDF4FHRF5GW6G5J63JKGJTGERTG';
-const currentBlockHash = [
-    {
-        amount: 10,
-        sender: 'FDADADN43FEF42FGGDS',
-        recipient: 'FH42345456GGWWEGTW'
-    },
-    {
-        amount: 30,
-        sender: 'DFARFJK3T5GWRER45',
-        recipient: 'SRVJWKLRWCRJT45CT4GJW'
-    },
-    {
-        amount: 200,
-        sender: 'FGGDSSGR45TY45G',
-        recipient: 'GWWEGTW54G54G4CGG'
-    }
-];
+//const previousBlockHash = 'AFHDF4FHRF5GW6G5J63JKGJTGERTG';
+//const currentBlockHash = [
+//    {
+//        amount: 10,
+//        sender: 'FDADADN43FEF42FGGDS',
+//        recipient: 'FH42345456GGWWEGTW'
+//    },
+//    {
+//        amount: 30,
+//        sender: 'DFARFJK3T5GWRER45',
+//        recipient: 'SRVJWKLRWCRJT45CT4GJW'
+//    },
+//    {
+//        amount: 200,
+//        sender: 'FGGDSSGR45TY45G',
+//        recipient: 'GWWEGTW54G54G4CGG'
+//    }
+//];
 // Testing the nonce function for proofOfWork
-let nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockHash)
+//let nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockHash)
 // Showing the nonce from the proofOfWork function
 //console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockHash));
 
 // Showing the hashBlock output
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockHash, nonce));
+//console.log(bitcoin.hashBlock(previousBlockHash, currentBlockHash, nonce));
 
 // Showing the hashBlock function
 //console.log(bitcoin.hashBlock(previousBlockHash, currentBlockHash, nonce));
 
-
+// Show the Genesis Block
+ console.log(bitcoin);
