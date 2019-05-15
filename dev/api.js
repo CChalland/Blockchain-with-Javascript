d5397947cee02790c237681d20298bf4e254c060
Cole Challand <ctchalland@gmail.com>
2019-05-15 09:13:47 -0500

    Transaction POST will create a new transaction which will return a the block index

--- dev/api.js
+++ dev/api.js
@@ -13,8 +13,8 @@ app.get('/blockchain', function(req, res) {
 });
 
 app.post('/transaction', function(req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}` });
 });
 
 app.get('/mine', function(req, res) {
@@ -23,4 +23,4 @@ app.get('/mine', function(req, res) {
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
-})
+});
