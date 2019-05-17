bbaeb9a86791f43961b099530ba35167e10aae2f
Cole Challand <ctchalland@gmail.com>
2019-05-17 10:43:13 -0500

    Building /register-and broadcast-node with the Promises outline

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const blockchain = require('./blockchain');
 const uuid = require('uuid/v1');
 const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid().split('-').join('');
 
@@ -44,7 +45,24 @@ app.get('/mine', function(req, res) {
 // Register a node and braodcast it the network
 app.post('/register-and-broadcast-node', function(req, res) {
   const newNodeUrl = req.body.newNodeUrl;
-  //....
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) == 1) bitcoin.networkNodes.push(newNodeUrl);
+
+  const regNodesPromises = [];
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + '/register-node',
+      methond: 'POST',
+      body: { newNodeUrl: newNodeUrl },
+      json: true
+    };
+
+    regNodesPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(regNodesPromises)
+  .then(data => {
+    // use the data....
+  });
 });
 
 
